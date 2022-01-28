4e964f27df8ab1c6812b0862612222c53833120f
Kmo <kmo6bf@gmail.com>
2022-01-28 11:48:13 +0900

    캡슐화 4번 문제 풀이

--- Encapsulation/problem04_myself.java
+++ Encapsulation/problem04_myself.java
@@ -1,12 +1,20 @@
 public void verifyEmail(String token) {
-    Member mem = findByToken(token);
+        Member mem = findByToken(token);
 
-    if (mem == null) throw new BadTokenException();
+        if (mem == null) throw new BadTokenException();
 
-    if (mem.getVerificationEmailStatus() == 2) {
-        throw new AlreadyVerifiedException();
-    } else {
-        mem.setVerificationEmailStatus(2);
+        mem.checkVerificationEmailStatus();
+        // ... 수정사항 DB 반영
+        }
+
+public class Member {
+    int verificationEmailStatus;
+
+    public void checkVerificationEmailStatus() {
+        if (verificationEmailStatus == 2) {
+            throw new AlreadyVerifiedException();
+        } else {
+            verificationEmailStatus = 2;
+        }
     }
-    // ... 수정사항 DB 반영
 }
--- Encapsulation/problem04_solve.java
+++ Encapsulation/problem04_solve.java
@@ -3,11 +3,28 @@ public void verifyEmail(String token) {
 
     if (mem == null) throw new BadTokenException();
 
-    if (mem.getVerificationEmailStatus() == 2) {
-        throw new AlreadyVerifiedException();
-    } else {
-        mem.setVerificationEmailStatus(2);
-    }
+    // 데이터를 직접 가져와서 판단하고 있다.
+    // 데이터를 직접 변경해주고 있다.
+    // 캡슐화를 위해 해당 메서드를 통째 로 Member클래스에 정의해준다.
+    mem.verifyEmail();
+
     // ... 수정사항 DB 반영
+}
+
+public class Member {
+    private int verificationEmailStatus;
+
+    public void verifyEmail() {
+        if (isEmailVerified()) {
+            throw new AlreadyVerifiedException();
+        } else {
+            verificationEmailStatus = 2;
+        }
+    }
+
+    // 메서드가 하나의 역할만 수행하도록 분
+    public boolean isEmailVerified() {
+        return verificationEmailStatus == 2;
+    }
 }
 

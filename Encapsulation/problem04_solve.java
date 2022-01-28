da3cb3173be188a34941e595670b0699a477e30f
Kmo <kmo6bf@gmail.com>
2022-01-28 11:32:55 +0900

    캡슐화 4번 문제 생성

---
+++ Encapsulation/problem04_solve.java
@@ -0,0 +1,13 @@
+public void verifyEmail(String token) {
+    Member mem = findByToken(token);
+
+    if (mem == null) throw new BadTokenException();
+
+    if (mem.getVerificationEmailStatus() == 2) {
+        throw new AlreadyVerifiedException();
+    } else {
+        mem.setVerificationEmailStatus(2);
+    }
+    // ... 수정사항 DB 반영
+}
+
